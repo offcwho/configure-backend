9a7c4be333889cb7eaaa51f74e825afcae0ed623
offcwho <nhnhnhbgbg@gmail.com>
2025-11-23 11:33:14 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import path from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000/',
+    origin: process.env.CLIENT_URL || 'https://configure-xucy.vercel.app/',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
