821773eafe13c98f0c1feb3195b5e8ab45b4dcae
offcwho <nhnhnhbgbg@gmail.com>
2025-11-23 12:43:18 +0300

    cookie

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,20 +1,23 @@
-// Импортируем зависимости
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
-
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor() {
-        super({
-            jwtFromRequest: ExtractJwt.fromExtractors([(req) => req.cookies['accessToken']]),
-            ignoreExpiration: false,
-            secretOrKey: "access-secret-key"
-        });
-    }
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt-cookie') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req) => {
+          if (!req || !req.cookies) return null;
+          return req.cookies['accessToken'];
+        },
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_ACCESS_SECRET || 'access-secret-key',
+    });
+  }
 
-    async validate(payload: any) {
-        return { userId: payload.sub, email: payload.email, role: payload.role };
-    }
+  async validate(payload: any) {
+    return { userId: payload.sub, email: payload.email, role: payload.role };
+  }
 }
