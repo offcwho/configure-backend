235363b6db27b678e4c2d438c37978e66c0e40e0
offcwho <nhnhnhbgbg@gmail.com>
2025-11-24 09:19:28 +0300

    update user

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { NestjsFormDataModule } from 'nestjs-form-data';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, NestjsFormDataModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
