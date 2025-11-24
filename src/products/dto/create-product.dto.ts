15ca7c371f0c2a8bcb60afec79c242407ab68dc7
offcwho <nhnhnhbgbg@gmail.com>
2025-11-24 09:44:22 +0300

    update user

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -2,19 +2,9 @@ import { IsString, IsInt } from 'class-validator';
 import { IsFile, IsFiles, MaxFileSize, HasMimeType, MemoryStoredFile } from 'nestjs-form-data';
 
 export class CreateProductDto {
-    @IsString()
     name: string;
-
-    @IsString()
     description: string;
-
-    @IsString()
     content: string;
-
-    @IsInt()
     price: number;
-
-    @IsFile() // если одно изображение
     images: MemoryStoredFile;
-    // или @IsFiles() для массива файлов
 }
