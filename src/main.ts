ce757da0f875f90cbb88aaeaa394ad4bc1062e81
offcwho <nhnhnhbgbg@gmail.com>
2025-11-24 04:01:00 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: process.env.CLIENT_URL || 'https://configure-xucy.vercel.app',
+    origin: [
+      process.env.CLIENT_URL || 'https://configure-xucy.vercel.app',
+      'http://localhost:3000',
+    ],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
