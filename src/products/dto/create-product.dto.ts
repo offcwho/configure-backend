db35533dd2b5bcdbfd188b205fa125ee8eae5ad8
offcwho <nhnhnhbgbg@gmail.com>
2025-11-24 08:26:22 +0300

    update user

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,17 @@
+import { Transform } from "class-transformer";
+import { IsInt, IsString } from "class-validator";
+
 export class CreateProductDto {
+    @IsString()
     name: string;
+
+    @IsString()
     description: string;
+
+    @IsString()
     content: string;
+
+    @IsInt()
+    @Transform(({ value }) => parseInt(value, 10))
     price: number;
 }
