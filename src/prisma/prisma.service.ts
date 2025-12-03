5818e9832cf60f24b0430fbca41657fbfdfb26df
offcwho <nhnhnhbgbg@gmail.com>
2025-12-03 14:51:30 +0300

    update

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,16 @@
+
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'src/generated/prisma/client';
+import 'dotenv/config'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
-    super({
-      adapter: {
-        url: process.env.DATABASE_URL,
-      },
+    const adapter = new PrismaPg({
+      url: process.env.DATABASE_URL
     });
+    super({ adapter });
   }
 
   async onModuleInit() {
