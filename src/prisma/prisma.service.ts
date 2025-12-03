90deb28c3f066dcfb4723e13993ddf455f78eec5
offcwho <nhnhnhbgbg@gmail.com>
2025-12-03 21:30:04 +0300

    upd

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,19 +4,10 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'src/generated/prisma/client';
 import 'dotenv/config'
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
-    const connectionString = `${process.env.DATABASE_URL}`
-
-    const adapter = new PrismaPg(
-      { connectionString },
-      { schema: 'mynewschema' }
-    );
-    super({ adapter });
-  }
-
   async onModuleInit() {
     console.log('Connecting to database...');
     await this.$connect();
