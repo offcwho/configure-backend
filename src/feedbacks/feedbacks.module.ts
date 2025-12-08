c4f17ec1e5d1f5a233cba0fae9b4060dbc3e1434
offcwho <nhnhnhbgbg@gmail.com>
2025-12-08 17:47:39 +0300

    update

--- src/feedbacks/feedbacks.module.ts
+++ src/feedbacks/feedbacks.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FeedbacksService } from './feedbacks.service';
 import { FeedbacksController } from './feedbacks.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [FeedbacksController],
   providers: [FeedbacksService],
 })
-export class FeedbacksModule {}
+export class FeedbacksModule { }
